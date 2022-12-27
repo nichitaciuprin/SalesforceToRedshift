dcbeedc53926810b1a99ef4ef0335ff701a0b43d
Nichita Ciuprin <nichitaciuprin@gmail.com>
2022-12-27 14:41:40 +0200

    Moved code to MigrateCsvFile method

--- SalesforceToRedshift/Program.cs
+++ SalesforceToRedshift/Program.cs
@@ -38,24 +38,32 @@ public static class Program
         var fileName = tableName+".csv";
         var csvFile = ExtractCsvFile(config.ZipFilesSearchPath,fileName,config.DirPath);
         FileHelper.PrintFileSize(csvFile);
-        using var redshiftClient = new RedshiftClient(config.RedshiftConfig);
-        using var s3Client = new S3Client(config.S3Config);
-        var sfdxClient = new SfdxClient(config.SalesforceUserName);
         CsvHelper2.Validate(csvFile);
         var oldCsvFile = csvFile;
         var newCsvFile = CsvHelper2.SplitByColumnSizeIfLargeValueExists(csvFile,RedshiftClient.VarcharMaxSizeInBytes);
         csvFile = newCsvFile == null ? oldCsvFile : newCsvFile;
+
+        MigrateCsvFile(config,csvFile);
+
+        if (newCsvFile != null)
+            File.Delete(newCsvFile);
+        File.Delete(oldCsvFile);
+        FileHelper.SaveCreate(saveFilePath,tableName);
+        Console.WriteLine($"DONE");
+    }
+    private static void MigrateCsvFile(Config config, string csvFile)
+    {
+        var fileName = Path.GetFileName(csvFile);
+        var tableName = Path.GetFileNameWithoutExtension(csvFile);
+        using var redshiftClient = new RedshiftClient(config.RedshiftConfig);
+        using var s3Client = new S3Client(config.S3Config);
+        var sfdxClient = new SfdxClient(config.SalesforceUserName);
         redshiftClient.CreateSchemaIfNotExists(config.RedsfhitTargetSchemaName);
         var lines = MigrationLogic.ColumnLines(tableName, csvFile, sfdxClient);
         redshiftClient.DropAndCreateTable(config.RedsfhitTargetSchemaName, tableName, lines);
         s3Client.UploadFile(fileName,csvFile);
         var s3filePath = $"{config.S3Config.BucketName}/{fileName}";
         redshiftClient.Copy(config.RedsfhitTargetSchemaName,tableName,s3filePath,config.S3Config);
-        if (newCsvFile != null)
-            File.Delete(newCsvFile);
-        File.Delete(oldCsvFile);
-        FileHelper.SaveCreate(saveFilePath,tableName);
-        Console.WriteLine($"DONE");
     }
     private static string ExtractCsvFile(string searchPath, string fileName, string extractPath)
     {
