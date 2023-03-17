18c31ad8e27001227cc2aeeb3a3a5d53583e1ff8
Nichita Ciuprin <nichitaciuprin@gmail.com>
2023-03-17 13:36:38 +0200

    UPDATE

--- SalesforceToRedshift/src/CsvFileExtracter.cs
+++ SalesforceToRedshift/src/CsvFileExtracter.cs
@@ -2,17 +2,6 @@ using System.IO.Compression;
 
 public static class CsvFilesExtracter
 {
-    // public static IEnumerable<string> GetCsvFiles(string searchPath, string extractDirectory, string[] exceptFileNames)
-    // {
-    //     var csvFileNames = GetFileNames(searchPath).Except(exceptFileNames);
-    //     foreach (var csvFileName in csvFileNames)
-    //     {
-    //         var filesWithSameName = ExtractFilesWithSameName(searchPath,csvFileName,extractDirectory);
-    //         var csvFile = Path.Combine(extractDirectory,csvFileName);
-    //         CsvHelper2.Merge(filesWithSameName,csvFile);
-    //         yield return csvFile;
-    //     }
-    // }
     public static string Extract(string searchPath, string extractDirectory, string fileName)
     {
         Directory.CreateDirectory(extractDirectory);
@@ -21,25 +10,6 @@ public static class CsvFilesExtracter
         CsvHelper2.Merge(filesWithSameName,csvFile);
         CsvHelper2.Validate(csvFile);
         return csvFile;
-    }
-    private static string[] ExtractFilesWithSameName(string searchPath, string fileName, string extractDirectory)
-    {
-        var zipPaths = Directory.GetFiles(searchPath,"*.zip",SearchOption.AllDirectories);
-        var list = new List<string>();
-        for (int i = 0; i < zipPaths.Length; i++)
-        {
-            var zipFile = zipPaths[i];
-            var targetFile = Path.Combine(extractDirectory,fileName)+$".{i}";
-            using var zip = ZipFile.OpenRead(zipFile);
-            foreach (var entry in zip.Entries)
-            {
-                if (entry.Name != fileName) continue;
-                entry.ExtractToFile(targetFile);
-                list.Add(targetFile);
-                i++;
-            }
-        }
-        return list.ToArray();
     }
     public static string[] GetFileNames(string searchPath)
     {
@@ -54,5 +24,24 @@ public static class CsvFilesExtracter
             .Distinct()
             .Select(x => Path.GetFileName(x))
             .ToArray();
+    }
+    private static string[] ExtractFilesWithSameName(string searchPath, string fileName, string extractDirectory)
+    {
+        var zipPaths = Directory.GetFiles(searchPath,"*.zip",SearchOption.AllDirectories);
+        var list = new List<string>();
+        for (int i = 0; i < zipPaths.Length; i++)
+        {
+            var zipFile = zipPaths[i];
+            var targetFile = Path.Combine(extractDirectory,fileName)+$".{i}";
+            using var zip = ZipFile.OpenRead(zipFile);
+            foreach (var entry in zip.Entries)
+            {
+                if (entry.Name != fileName) continue;
+                entry.ExtractToFile(targetFile,true);
+                list.Add(targetFile);
+                i++;
+            }
+        }
+        return list.ToArray();
     }
 }
--- SalesforceToRedshift/src/Program.cs
+++ SalesforceToRedshift/src/Program.cs
@@ -7,7 +7,8 @@ public static class Program
 
         using var redshiftClient = new RedshiftClient(config.ConfigRedshift);
         using var s3Client = new S3Client(config.ConfigS3);
-        var sfdxClient = SalesForceClient.CreateFromSFDX(config.SalesforceUserName);
+        var salesForceClient = SalesForceClient.CreateFromSFDX(config.SalesforceUserName);
+
         var fileNamesDone = File.ReadAllLines(config.FileNamesDoneFile);
         var fileNamesTodo = CsvFilesExtracter.GetFileNames(config.LocalZipFilesSearchPath).Except(fileNamesDone);
 
@@ -27,13 +28,13 @@ public static class Program
 
                 var fileName = Path.GetFileName(csvFile);
                 var tableName = Path.GetFileNameWithoutExtension(csvFile);
-                var lines = MigrationLogic.CreateSqlCreateLines(tableName, sfdxClient);
+                var lines = MigrationLogic.CreateSqlCreateLines(tableName, salesForceClient);
                 redshiftClient.CreateSchemaIfNotExists(config.ConfigRedshift.SchemaName);
                 redshiftClient.DropAndCreateTable(config.ConfigRedshift.SchemaName, tableName, lines);
 
-                var s3file = $"{config.S3WorkingDirectory}/{fileName}";
-                s3Client.UploadFile(csvFile,s3file);
-                redshiftClient.Copy(config.ConfigRedshift.SchemaName,tableName,s3file,config.ConfigS3);
+                var s3CsvFile = $"{config.S3WorkingDirectory}/{fileName}";
+                s3Client.UploadFile(csvFile,s3CsvFile);
+                redshiftClient.Copy(config.ConfigRedshift.SchemaName,tableName,s3CsvFile,config.ConfigS3);
 
                 File.AppendAllLines(config.FileNamesDoneFile, new [] { fileNameTodo } );
             }
